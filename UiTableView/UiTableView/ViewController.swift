754eadd564cdcd2e4638e349e308018d39927bcb
Alejandro <acervantest83@gmail.com>
2024-03-22 15:04:23 -0700

    Delegate methods defined

--- UiTableView/UiTableView/ViewController.swift
+++ UiTableView/UiTableView/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemMint
     }
 
+    func configure() {
+        view.addSubview(tableView)
+    }
 
 }
+
+// MARK: - Delegate Methods
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableViewDelegates() {
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            return UITableViewCell()
+    }
+}
+
 
