926f988707ef12b6dfab93898fac55b8bcf7fff0
Alejandro <acervantest83@gmail.com>
2024-03-22 14:51:25 -0700

    No story board initial setup

---
+++ UiTableView/UiTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UiTableView
+//
+//  Created by Alejandro Cervantes on 2024-03-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+
+
+}
+
